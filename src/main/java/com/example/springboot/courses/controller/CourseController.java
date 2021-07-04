f0b34d3dba7086a17ff2c2a4307ddd43af1c9689
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-07-04 21:48:46 +0700

    end of 344: playing with Spring boot

--- src/main/java/com/example/springboot/courses/controller/CourseController.java
+++ src/main/java/com/example/springboot/courses/controller/CourseController.java
@@ -19,6 +19,14 @@ public class CourseController {
 
     @GetMapping("/courses")
     public List<Course> getAllCourses() {
-        return List.of(new Course(1, "Learn Java", "Bkacad"));
+        return List.of(new Course(1, "Learn Java", "Bkacad"),
+                new Course(2, "Learn full stack with Angular and React", "Bkacad"));
     }
+
+    @GetMapping("/courses/1")
+    public Course getCourseDetails() {
+        return new Course(1, "Learn Java", "Bkacad");
+    }
+
+
 }
