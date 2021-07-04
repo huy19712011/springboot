b5c98943fdfe423a72c46621f5f18c8fb2d31790
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-07-04 21:06:18 +0700

    end of 340: Hello World API with Spring Boot

---
+++ src/main/java/com/example/springboot/courses/controller/CourseController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.springboot.courses.controller;
+
+import com.example.springboot.courses.bean.Course;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author huynq
+ */
+@RestController
+public class CourseController {
+
+    @GetMapping("/courses")
+    public List<Course> getAllCourses() {
+        return List.of(new Course(1, "Learn Java", "Bkacad"));
+    }
+}
