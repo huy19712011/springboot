5df7deb803b5b805d7eeba3b2f27c90638a2b27d
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-07-06 10:10:48 +0700

    end of 354 part 2: update data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  huynq
+ * Created: Jul 5, 2021
+ */
+insert into course
+values(1001, 'Bkacad', 'Java');
+insert into course
+values(1002, 'Bkacad', 'Spring Boot');
+insert into course
+values(1003, 'Bkacad', 'Angular');
+insert into course
+values(1004, 'Bkacad', 'ReactJs');
